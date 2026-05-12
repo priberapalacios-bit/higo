57ffa72c6f584d0b09f5bee138672ee59ffff7da
priberapalacios-bit <p.riberapalacios@ied.edu>
2026-05-12 19:36:43 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+APP_NAME = "RutaCalma Madrid"
+
+DATABASE_PATH = "/tmp/rutacalma.db"
+
+DEFAULT_NOISE_SENSITIVITY = 7
+DEFAULT_CROWD_SENSITIVITY = 8
+DEFAULT_UNCERTAINTY_TOLERANCE = 3
+
+MAX_STRESS_SCORE = 100
+
+INCIDENT_OPTIONS = [
+    "Sin incidencias",
+    "Metro cerrado",
+    "Retraso severo",
+    "Alta ocupación",
+]
+
+SAFE_STRESS_LIMIT = 45
+HIGH_STRESS_LIMIT = 70
